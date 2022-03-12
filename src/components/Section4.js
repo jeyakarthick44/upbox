f7b5e6a1925000256af8987eac5ea6a1e1d4a044
jeyakarthick44 <jeyakarthick408@gmail.com>
2022-03-12 23:53:26 +0530

    Completed footer

--- src/components/Section4.js
+++ src/components/Section4.js
@@ -1,5 +1,9 @@
 import React from "react";
 import { Box } from "@chakra-ui/react";
+import img1 from "../images/img1.png";
+import img2 from "../images/img2.png";
+import img3 from "../images/img3.png";
+import img4 from "../images/img4.png";
 
 import { SimpleGrid } from "@chakra-ui/react";
 const Section4 = () => {
@@ -8,12 +12,38 @@ const Section4 = () => {
       <div className="section4">
         <p className="social">Social Posts</p>
       </div>
-      <SimpleGrid columns={[1, 2, 3]} spacing={10}>
-        <Box bg="tomato" height="80px"></Box>
-        <Box bg="tomato" height="80px"></Box>
-        <Box bg="tomato" height="80px"></Box>
-        <Box bg="tomato" height="80px"></Box>
-        <Box bg="tomato" height="80px"></Box>
+      <SimpleGrid
+        columns={[1, 2, 4]}
+        ml={5}
+        mr={5}
+        spacing="20px"
+        className="grid"
+        style={{ marginTop: "-500px" }}
+      >
+        <Box>
+          <img className="social-img" src={img1} alt="" />
+          <h1 className="social-head">
+            How to rock the lip look that turns heads
+          </h1>
+        </Box>
+        <Box>
+          <img className="social-img" src={img2} alt="" />
+          <h1 className="social-head">
+            How to rock the lip look that turns heads
+          </h1>
+        </Box>
+        <Box>
+          <img className="social-img" src={img3} alt="" />
+          <h1 className="social-head">
+            How to rock the lip look that turns heads
+          </h1>
+        </Box>
+        <Box>
+          <img className="social-img" src={img4} alt="" />
+          <h1 className="social-head">
+            How to rock the lip look that turns heads
+          </h1>
+        </Box>
       </SimpleGrid>
     </>
   );
